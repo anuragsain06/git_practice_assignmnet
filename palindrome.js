fd1e8073e7f4baf96c81a479a45907bbe579adb5
Mrkishansharma <kishansharma6377@gmail.com>
2022-11-30 22:45:05 +0530

    i update code

--- palindrome.js
+++ palindrome.js
@@ -1,11 +1,11 @@
 let str="naman"
-let rev = 0
+let rev = ''
 
 for(let i=str.length-1; i>=0; i--){
     rev = rev + str[i]
 }
 
-if(rev=str){
+if(rev==str){
     console.log("Yes");
 }else{
     console.log("No");
